ea14af73489d3fb68680b103646c7bdb2cde0a86
Ivanna Rodríguez Lobo <44304361+ivannarodriguez@users.noreply.github.com>
2023-10-21 19:43:31 +0000

    finished dynamic exercie

--- app.rb
+++ app.rb
@@ -53,7 +53,7 @@ get("/dice/5/4") do
   erb(:five_four)
 end
 
-get("dynamic/:number_of_dice/:how_many_sides") do
+get("/dice/:number_of_dice/:how_many_sides") do
   @num_dice = params["number_of_dice"].to_i
   @num_sides = params["how_many_sides"].to_i
   @rolls = []
